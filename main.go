e0b679b924ff518c408fa2416140258a7f2ca659
Harish Kukreja <harish.kukreja@gmail.com>
2022-01-22 16:27:41 -0500

    Implement first stage of coffee machine exercise

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")
+	lines := []string{
+		"Starting to make a coffee",
+		"Grinding coffee beans",
+		"Boiling water",
+		"Mixing boiled water with crushed coffee beans",
+		"Pouring coffee into the cup",
+		"Pouring some milk into the cup",
+		"Coffee is ready!",
+	}
+
+	for _, line := range lines {
+		fmt.Println(line)
+	}
 }
