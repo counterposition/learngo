859c76b1ae943f0266c75ea81ba38ebf7d7f7ed4
Harish Kukreja <harish.kukreja@gmail.com>
2023-05-21 16:02:29 -0400

    hello world using samber/do

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import "github.com/samber/do"
+import "go.uber.org/zap"
 
 func main() {
-	fmt.Println("Hello, world.")
+	container := do.New()
+	defer container.Shutdown()
+
+	do.Provide(container, loggerService)
+	do.Provide(container, newHelloService)
+	run := do.MustInvoke[*helloService](container)
+
+	run.sayHello(container)
+}
+
+func loggerService(i *do.Injector) (*zap.Logger, error) {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		return nil, err
+	} else {
+		return logger, nil
+	}
+}
+
+type helloService struct{}
+
+func newHelloService(i *do.Injector) (*helloService, error) {
+	return &helloService{}, nil
+}
+
+func (hs *helloService) sayHello(i *do.Injector) error {
+	log := do.MustInvoke[*zap.Logger](i)
+	log.Info("hello world", zap.Int("count", 43))
+	return nil
 }
